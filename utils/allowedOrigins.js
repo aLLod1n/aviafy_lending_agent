6c2d6719f0c440a424018bfec9af168038cbf2bc
sibel <pipiaalex7@gmail.com>
2025-04-15 18:51:07 +0400

    uupp

--- utils/allowedOrigins.js
+++ utils/allowedOrigins.js
@@ -1,1 +1,5 @@
-export const allowedOrigins = ["http://localhost:3000"];
+export const allowedOrigins = [
+  "http://localhost:3000",
+  "https://bot-landing-sigma.vercel.app/en",
+  "https://bot-landing-sigma.vercel.app",
+];
