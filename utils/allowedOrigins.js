a354417dfe0932e5f86e8f091ece3586f7ab0c75
sibel <pipiaalex7@gmail.com>
2025-04-26 22:11:47 +0400

    origin

--- utils/allowedOrigins.js
+++ utils/allowedOrigins.js
@@ -1,5 +1,7 @@
 export const allowedOrigins = [
   "http://localhost:3000",
+  "https://petbuddy-one.vercel.app/en",
+  "https://petbuddy-one.vercel.app",
   "https://bot-landing-sigma.vercel.app/en",
   "https://bot-landing-sigma.vercel.app",
 ];
